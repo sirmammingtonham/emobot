c8abdfd877ff0bf5ad56ed89ba80eaae6194e21a
sirmammingtonham <ethanj217@gmail.com>
2020-12-02 11:04:31 -0800

    add still image face detection

--- face_recognition/face_recognition.py
+++ face_recognition/face_recognition.py
@@ -15,6 +15,30 @@ class FacialRecognition:
 
         # we can change this to whatever the dataset is scaled to
         self.face_scale = (48, 48) 
+    
+    def run_detection_still(self, image):
+        _, img = cv2.imread(image)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        detected = self.face_detection.detectMultiScale(
+            gray, **self.settings)
+
+        for x, y, w, h in detected:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (245, 135, 66), 2)
+            cv2.rectangle(img, (x, y), (x+w//3, y+20), (245, 135, 66), -1)
+            face = gray[y+5:y+h-5, x+20:x+w-20]
+            face = cv2.resize(face, self.face_scale)
+            face = face/255.0
+
+            yield face
+
+        cv2.imshow('Facial Expression', img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        while True:
+            if cv2.waitKey(5) != -1:
+                break
+
+        cv2.destroyAllWindows()
 
     def run_detection_loop(self):
         camera = cv2.VideoCapture(0)
